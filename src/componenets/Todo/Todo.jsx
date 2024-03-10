ab9bb343cb7ef17d87c1e955b0256b4c774ecf17
mccartheney <mccartheney@gmail.com>
2024-03-10 18:48:27 +0000

    TodoInput functionalitie and stylization finalized

--- src/componenets/Todo/Todo.jsx
+++ src/componenets/Todo/Todo.jsx
@@ -1,10 +1,17 @@
+// import react things
+import { useState } from "react";
+
 // import components
 import TodoHeader from "./TodoHeader/TodoHeader";
+import TodoInput from "./TodoInput/TodoInput";
 
 // Main Component Todo
 const Todo = (props) => {
     // get theme states from props 
     const {theme, setTheme} = props;
+
+    const [todoList, setTodoList] = useState([]);
+    console.log(todoList);
 
     // return main todo functionalities
     return (
@@ -12,6 +19,10 @@ const Todo = (props) => {
             <div className="main_component_todo_todos">
                 {/* todo header */}
                 <TodoHeader theme={theme} setTheme={setTheme}/>
+
+                {/* create new todo */}
+                <TodoInput setTodoList={setTodoList} todoList={todoList}/>
+
             </div>
         </div>
     );
--- src/componenets/Todo/TodoInput/TodoInput.jsx
+++ src/componenets/Todo/TodoInput/TodoInput.jsx
@@ -1,5 +1,57 @@
-const TodoInput = (props) => {
+// import react stuff
+import { useId, useState } from "react";
 
+// funtion to create new todo
+const AddNewTodo = (event, value, id, setTodoList, todoList) => {
+    // dont reload page when add new todo
+    event.preventDefault();
+
+    // create new todo obj
+    const newTodo = {
+        value : value,
+        id : id,
+        compleated : false
+    }
+
+    // with todo state add todo to their list
+    setTodoList(oldTodoList => {
+        return ([...oldTodoList, newTodo]);
+    });
+}   
+
+// main component
+const TodoInput = (props) => {
+    // get todo states from props
+    const {todolist, setTodoList} = props
+
+    // create states for new todo input
+    const [inputValue, setInputValue] = useState("")
+
+    // create new id
+    const id = useId();
+
+    // return form
+    return (
+        // when submit that form:
+        //      add new todo with arguments:
+        //          -event, input value, id, listStates
+        <form className="main_component_todo_todos_inputTodo" 
+            onSubmit={(event) => AddNewTodo(event, inputValue, id, setTodoList, todolist)}>
+            <input type="marker" disabled="disabled" className="main_component_todo_todos_inputTodo-marker" />
+
+            {/* input with the value for new todo */}
+            <input 
+                type="text" 
+                className="main_component_todo_todos_inputTodo-input" 
+                placeholder="Create a new todo ..." 
+                onChange={(event) => {setInputValue(event.target.value)}}
+                value={inputValue}
+            />
+
+            <input type="submit" hidden />
+        </form>
+    )
 };
 
+// export TodoInput
 export default TodoInput;
